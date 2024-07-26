73e187351448d02fec81e934cc5255b35a39da9b
Shruti Verma <96043924+Hiddenworld26@users.noreply.github.com>
2024-07-26 22:07:33 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {createPost , getPosts , getPostById , updatePost , deletePost  } = require('../controllers/postController');
 
+const protect = require('../middleware/authMiddleware');
 
-
-router.post('/create', createPost);
-router.get('/', getPosts);
-router.get('/:id', getPostById);
-router.put('/update/:id', updatePost);
-router.delete('/delete/:id', deletePost);
+router.post('/create', protect , createPost);
+router.get('/', protect, getPosts);
+router.get('/:id', protect , getPostById);
+router.put('/update/:id', protect, updatePost);
+router.delete('/delete/:id', protect, deletePost);
 
 module.exports = router;
